33307cc22126e18fb5979c18782ad3c2af2487f9
RAndrade <ruiwilson7@gmail.com>
2014-09-05 15:56:15 +0100

    Criada pasta para guardar os perfis de cada praia
    Criado um sistema de guardar comentarios das praias em ficheiro txt e leitura do mesmo

--- AllgarveBeachProj/PerfilPraia/Olhao_PraiaDosTesos.aspx.cs
+++ AllgarveBeachProj/PerfilPraia/Olhao_PraiaDosTesos.aspx.cs
@@ -8,41 +8,33 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace AllgarveBeach
+namespace AllgarveBeach.PerfilPraia
 {
-    public partial class Galleries : System.Web.UI.Page
+    public partial class Olhao_PraiaDosTesos : System.Web.UI.Page
     {
-        Ficheiros f = new Ficheiros();
+
+Ficheiros f = new Ficheiros();
 
         protected void Page_Load(object sender, EventArgs e)
         {
-        /*    btnocultartesos.Visible = true;
-            btnvertesos.Visible = false;
-
-            string path = "C://temp//tesos//coment.txt";
-            if (!string.IsNullOrEmpty(path))
-            {
-                string[] readText = File.ReadAllLines(path);
-                System.Text.StringBuilder strbuild = new StringBuilder();
-                foreach (string s in readText)
-                {
-                    strbuild.Append(s);
-                    strbuild.AppendLine();
-                }
-                lttesos.Text = strbuild.ToString();
-            }*/
+            btnocultartesos.Visible = false;
         }
 
         protected void EnviarTesos_Click(object sender, EventArgs e)
         {
+            btnvertesos.Visible = true;
    /*         if (Session.SessionID)
             {
             lttesos.Text += "<br>" + "Nick disse: " + ComentTesos.Text;
             }
             else+*/
-            lttesos.Text += "<br>Data: " + System.DateTime.Today + "<br>Anónimo> " + ComentTesos.Text + " <<br><hr>";
 
+            lttesos.Text += "<table text-align='left' border='1' width='100%'><tr>" +
+             "<td style='text-align:left' bgcolor=#ccff00 align=center><b>Data: </b>" + System.DateTime.Today +
+             "</tr><tr><td style='text-align:left' bgcolor=#cccccc align=center height='30' width='100%'>Anónimo> " + ComentTesos.Text + " </td></tr></table>";
 
+            
+            
             System.IO.Directory.CreateDirectory("C://temp//tesos");
             f.CriarComentarioFile("C://temp//tesos//coment.txt",lttesos.Text);
             lttesos.Text = "";
@@ -72,7 +64,5 @@ namespace AllgarveBeach
                 lttesos.Text = strbuild.ToString();
             }
         }
-
- 
     }
 }
